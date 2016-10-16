e490acb8f0a51fa29530045160726e182842830e
natal <natalia.donca@gmail.com>
2016-10-16 03:45:58 +0100

    preenchomento da tabela
    Preenche comform o nivel de deficuldade e faz umas verificaçoes

--- game/js/main.js
+++ game/js/main.js
@@ -5,17 +5,11 @@
 
 (function(){
 
-var selectedMode = $('');
+var selectedMode;
 
   var panelAutores = $('#authors-section');
 	var autores = $('#authors-section').children('div');
-  var celulas = $('.dad-cell');
-
- // console.log(celulas);
-
-  var url = 'http://198.211.118.123:8080/board/easy';
-    //url +=encodeURI(searchText);
-     $.get(url,processAPIResult);
+  var jogoInicial;
 
   $('h3',autores.eq(0)).text('2140792');
   $('p',autores.eq(0)).text('????? Gupo ?????');
@@ -23,19 +17,92 @@ var selectedMode = $('');
   autores.eq(2).remove();
   autores.eq(3).remove();
 
-	console.log(autores.lenght);
+	//console.log(autores.lenght);
 
-  function processAPIResult(result, status){
-    var data = result;
-    console.log(data);
+  function processAPIResult(jogoInicial, status){
+     if(jogoInicial.Response === 'False'){
+    
+    }else{
+     $.each( jogoInicial, function( key, value ) {
+      var a = '[data-column='+value.column+'][data-line='+value.line+']';
+      $(a).val(value.value);
+      });
+    }
   }
 
-  
-    var url = 'http://198.211.118.123:8080/board/easy';
-    //url +=encodeURI(searchText);
-     $.get(url,processAPIResult);
+  function callAPI(modo='easy'){
+    var url = 'http://198.211.118.123:8080/board/'+modo;
+    $.get(url,processAPIResult)
+    .fail(function(jogoInicial, status, request) {
+      
+    })
+    .always(function(){
+
+    });
+  }
+
+function limparTabuleiro(){
+ var cell = $('[data-column][data-line]');
+  $.each( cell, function( key, value ) {
+   value.value = null;
+    // value = 'empty';
+      })
+}
+
+  function newGame(){
+    limparTabuleiro();
+    var modo = $('option:selected',selectedMode).text();
+    callAPI(modo);
+  }
+
+  function validarLinha(cell){
+    var line = cell.getAttribute('data-line');
+    var a = $('[data-line='+line+']');
+    //console.log(a);
+      $.each( a, function( key, value ) {     
+        if(value.value == cell.value ) 
+        {         
+         $(value).css("background-color", "yellow");
+        }
+      })
+  }
+
+  function validarColumn(column){
+
+  }
+
+   function validarQuadrante(quadrante){
     
+  }
+
+  function actualiza(){
+  $('input', '.dad-cell').on('keyup', function (e) {
+    if(this.value<1 || this.value > 9){
+        this.value = null; 
+    }   
+    validarLinha(this);
+    //console.log(this.getAttribute('data-line'));
+    validarColumn(this.getAttribute('data-column'));
+    //console.log(this.getAttribute('data-column'));
+    //validarQuadrante(); 
+    e.preventDefault()
+    })
+  }
+
+   $(function(){
+    // DOM READY
+    callAPI();
+    $('#btn-new').click(newGame);
+    actualiza();
+    
+  });
 
 
+//var selector = $( ".form-control" ).selectmenu( "select-mode" );
+//console.log(selector);
 
+//$('input', '.dad-cell').on('keyup', function (e) {
+               
+//e.preventDefault()
+//    }
 })();
